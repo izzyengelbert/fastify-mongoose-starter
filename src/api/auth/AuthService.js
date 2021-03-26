2ac43c1d06adeb53c9e5ff767cd06ef65c6713b6
izzyengelbert <izzyengelbert@pop-os.localdomain>
2021-03-26 23:21:58 +0700

    Refactor authenticateUser

--- src/api/auth/AuthService.js
+++ src/api/auth/AuthService.js
@@ -10,17 +10,16 @@ export default class AuthService {
 
   async authenticateUser(username, password) {
     const user = await this._User.findUserByCredentials(username);
+    if (!user) throw new IncorrectCredentials();
     const { password: userPassword, ...userData } = user;
     const validPassword = await validatePassword(password, userPassword);
-    if (user && validPassword) {
-      const payload = { user: userData };
-      const response = {
-        token: jwt.sign(payload, config.secret, { expiresIn: '1h' }),
-        userId: user.id,
-        username: user.username
-      };
-      return response;
-    }
-    throw new IncorrectCredentials();
+    if (!validPassword) throw new IncorrectCredentials();
+    const payload = { user: userData };
+    const response = {
+      token: jwt.sign(payload, config.secret, { expiresIn: '1h' }),
+      userId: user.id,
+      username: user.username
+    };
+    return response;
   }
 }
